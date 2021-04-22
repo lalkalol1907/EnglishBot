f3fc70495f0e4ab9abdbc855f98f294611a202f8
lalkalol1907 <kostyaleonov13@yandex.ru>
2021-04-22 15:25:16 +0300

    translated to eng

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ def KBDGenerator(array):
 
 @bot.message_handler(commands=['start'])
 def Start(message):
-    bot.send_message(message.from_user.id, "Какой блок ты хочешь отработать?",
+    bot.send_message(message.from_user.id, "Choose block:",
                      reply_markup=KBDGenerator(['Listening','Reading','Grammar','Vocabulary']))
     quiz = Quiz()
+
     bot.register_next_step_handler(message, quiz.WhichQuiz)
 
 @bot.message_handler(content_types=['text'])
 def TextMessage(message):
-    if message.text == "Начать тест":
+    if message.text == "Start test":
         Start(message)
 
 
@@ -50,25 +51,30 @@ class Quiz:
         if message.text == "Listening":
             self.type = "Listening"
         elif message.text == "Reading":
-            self.type = "Reading"
+            self.type = "Readi#ng"
         elif message.text == "Grammar":
             self.type = "Grammar"
         elif message.text == "Vocabulary":
             self.type = "Vocabulary"
+        elif message.text == '/start':
+            Start(message)
         self.question = Question(self.type)
         self.questions = self.question.getQuestion()
         random.shuffle(self.questions)
         self.__Quiz(message)
 
     def __Quiz(self, message):
-        if message.text != "Закончить тест":
+        if message.text != "End test":
             if self.current_question:
                 self.__AnsCheck(message)
             if len(self.questions) > self.i:
                 bot.send_message(message.from_user.id, text=f"{self.questions[self.i]}",
-                                reply_markup=KBDGenerator(['Закончить тест']))
+                                reply_markup=KBDGenerator(['End test']))
                 if self.question.HasUrl(self.questions[self.i]):
-                    bot.send_voice(message.from_user.id, voice=self.question.getUrl(self.questions[self.i]))
+                    try:
+                        bot.send_voice(message.from_user.id, voice=self.question.getUrl(self.questions[self.i]))
+                    except:
+                        pass
                 self.current_question = self.questions[self.i]
                 self.i += 1
                 bot.register_next_step_handler(message, self.__Quiz)
@@ -97,10 +103,10 @@ class Quiz:
 
     def __CompareWith(self, userID, flag, *result):
         if flag == 'last':
-            textflag = "последнего"
+            textflag = "last"
             Res = self.DB.GetRes(flag, userID)
         elif flag == 'avg':
-            textflag = "среднего"
+            textflag = "average"
             Res = self.DB.GetRes(flag ,userID)
         if not Res or Res == 0:
             return ""
@@ -108,11 +114,11 @@ class Quiz:
             CurRes = (result[0]/result[1])*100
             delta = abs(round(Res - CurRes))
             if Res > CurRes:
-                return f"\nТвой результат хуже {textflag} на {delta}%"
+                return f"\nYour current result worse than your {textflag} result by {delta}%"
             elif Res < CurRes:
-                return f"\nТвой результат лучше {textflag} на {delta}%"
+                return f"\nYour current result better than your {textflag} result by {delta}%"
             else:
-                return f"\nТвой результат не изменился относительно {textflag}"
+                return f"\nYour current result is the same as your {textflag} result"
         except ZeroDivisionError:
             return ""
 
@@ -120,9 +126,9 @@ class Quiz:
         compare_ans = ""
         for i in range(len(self.user_answers)):
             compare_ans += f"{i+1}) Correct: {self.correct_answers[i]}  Your: {self.user_answers[i]}\n"
-        bot.send_message(message.from_user.id, f"""Тест закончен, результат:{self.counter} из {self.additional_counter}
+        bot.send_message(message.from_user.id, f"""Test finished, your result: {self.counter} / {self.additional_counter}
         {self.__CompareWith(message.from_user.id, 'last', self.counter, self.additional_counter)}{self.__CompareWith(message.from_user.id, 'avg', self.counter, self.additional_counter)}
-        \n\n{compare_ans}""", reply_markup=KBDGenerator(['Начать тест']))
+        \n\n{compare_ans}""", reply_markup=KBDGenerator(['Start test']))
         self.question.DB.addUserResult(message.from_user.id, self.counter, self.additional_counter)
         self.__Default()
 
